c27d83ccfda57976929e043cf95cda3367268de3
Steve Barrett <steven122185@gmail.com>
2019-11-18 18:24:35 -0500

    Created the register component, authed axios and router

---
+++ src/Components/Register/Register.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import axios from "axios";
+import config from "../../config/index";
+import {Input, Button} from '@material-ui/core';
+
+import 'Register.css';
+
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
+      password2: "",
+      loading: false
+    };
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    // TODO: Verify passwords match and add type checking for password security
+
+    const credentials = {
+      username: this.state.username,
+      password: this.state.password,
+      password2: this.state.password2
+    };
+
+    this.setState({
+      loading: true
+    });
+
+    axios
+      .post(`${config.apiUrl}/api/registration/`, credentials)  // URL will need updating when Django is complete
+      .then(res => {
+        // SET KEY TO localStorage?        
+        localStorage.setItem("authToken", res.data.key);
+        this.setState({
+          username: "",
+          password: "",
+          password2: "",
+          loading: false
+        });
+        // Route to game when component is complete and router is setup
+        this.props.history.push(`/`);
+      })
+      .catch(err => {
+        console.log(err.response.data);
+      });
+  };
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div class="register-container">
+        <form class="register-form">
+          <Input
+            onChange={this.onChange}
+            name="username"
+            placeholder="username..."
+            value={this.state.username}
+          />
+          <Input
+            onChange={this.onChange}
+            name="password"
+            placeholder="password..."
+            value={this.state.password}
+          />
+          <Input
+            onChange={this.onChange}
+            name="password2"
+            placeholder="confirm password..."
+            value={this.state.password2}
+          />
+          <Button onSubmit={this.onSubmit} />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Register;
---
+++ src/Router.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
+
+// * Checks if a user is authorized to access the game.
+const isAuthorized = () => {
+  return Boolean(localStorage.getItem("authToken"));
+};
+
+
+//  Creates a Protected Route from a passed in component. 
+//  Only renders if user is Authorized. Otherwise redirects user to login.
+//  returns Component (if auth) or Redirect (to login) 
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        isAuthorized() ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{ pathname: "/login" }} />
+        )
+      }
+    />
+  );
+};
+
+/**
+ * Sets public and private routes
+ * /register and /login are currently public. The main landing page will only be accessible to authenticated users.
+ *  return switch component routing users throughout app
+ */
+const Router = () => {
+  return (
+    <Switch>
+      <Route path="/register" component={Register} />
+      <Route path="/login" component={Login} />      
+      {/* <ProtectedRoute path="/" component={} />  render the game in a protected route*/}
+    </Switch>
+  );
+};
+
+export default Router;
